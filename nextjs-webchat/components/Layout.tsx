abe18b090104bae780196b0272634c54680ad6dc
mengru <4momolmr@gmail.com>
2021-09-29 23:01:14 +0800

    add tailwind

---
+++ nextjs-webchat/components/Layout.tsx
@@ -0,0 +1,12 @@
+
+import { NextComponentType } from 'next'
+
+
+export const Layout: NextComponentType = () => {
+    return (
+        <div className="flex">
+            <div>ss</div>
+            <div>xx</div>
+        </div>
+    )
+}
