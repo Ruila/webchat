db050253f273349f0f176f832fa7f3d59eb8c128
mengru <4momolmr@gmail.com>
2021-09-30 23:28:42 +0800

    add left side bar

--- nextjs-webchat/components/Layout.tsx
+++ nextjs-webchat/components/Layout.tsx
@@ -1,12 +1,17 @@
 
 import { NextComponentType } from 'next'
-
+import PersonIcon from '@mui/icons-material/Person';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 export const Layout: NextComponentType = () => {
     return (
-        <div className="flex">
-            <div>ss</div>
-            <div>xx</div>
+        <div className="flex justify-center items-center w-full h-full py-8">
+            <div className="w-[800px] h-[600px] bg-gray-200">
+                <div className="h-full bg-gray-900 w-[80px] flex flex-col items-center">
+                    <PersonIcon className="my-8" sx={{ color: "#fff" }} />
+                    <ChatBubbleIcon sx={{ color: "#fff" }} />
+                </div>
+            </div>
         </div>
     )
 }
